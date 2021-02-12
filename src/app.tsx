455f9af090b71b16c909c66b133134435b0a1439
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2021-02-13 00:22:31 +0100

    feat: Add React

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React, {FC} from "react";
+
+const App: FC = () => {
+  return <p>Hello, World!</p>
+}
+
+export default App;
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-function main() {
-  const pElement = document.createElement("p");
-
-  pElement.innerText = "Hello, World!";
-
-  document.body.appendChild(pElement);
-}
-
-main();
