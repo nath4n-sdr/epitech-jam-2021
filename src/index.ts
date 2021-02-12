9367a78cf5ae1bdf72e1786e7071fc2008356eda
Lucas Arsene <lucas.arsene@epitech.eu>
2021-02-13 00:03:37 +0100

    feat: adding base html

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-function main() {
-  const pElement = document.createElement("p");
-
-  pElement.innerText = "Hello, World!";
-
-  document.body.appendChild(pElement);
-}
+function main() {}
 
 main();
