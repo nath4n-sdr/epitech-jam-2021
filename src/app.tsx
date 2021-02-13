feb619f16ae712cb795fc1c67c1d775024ab0ac3
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2021-02-13 14:54:54 +0100

    feat: Add face-api camera face mesh (with key points visual and expression)

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useRef, useState } from "react";
 import { CameraSelectComp } from "./components/camera/select";
-import { FaceCamera2Comp } from "./components/camera/face-2";
+import { FaceCamera2Comp } from "./components/camera/face-camera-2";
+import { FaceVideoComp } from "./components/camera/face-video";
 
 const App: FC = () => {
   const [deviceId, setDeviceId] = useState<string>();
@@ -12,6 +13,8 @@ const App: FC = () => {
 
   return (
     <div>
+      <FaceVideoComp url={"images/monkey2.mp4"} />
+      <FaceVideoComp url={"images/monkey3.mp4"} />
       {deviceId ? (
         <FaceCamera2Comp
           constraints={{ video: { deviceId } }}
